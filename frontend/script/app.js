e28666c6f8c551fa600a1e0533df4aebb3ac784b
joaomarcosvs <joaomarcos.vieirasantos@gmail.com>
2026-01-16 11:27:41 -0300

    updated table and data

--- frontend/script/app.js
+++ frontend/script/app.js
@@ -14,14 +14,27 @@
         ];
 
         $scope.contatos = [
-            { nome: "Alanna", telefone: "5199864235", data: new Date(), operadora: $scope.operadoras[0] },
-            { nome: "Joao M", telefone: "5199995555", data: new Date(), operadora: $scope.operadoras[3] },
-            { nome: "Bia", telefone: "5199993333", data: new Date(), operadora: $scope.operadoras[1] },
-            { nome: "Eduardo", telefone: "5199994444", data: new Date(), operadora: $scope.operadoras[2] }
+            { nome: "Alanna", telefone: "5199864235", data: new Date(2025, 0, 12), operadora: $scope.operadoras[0] },
+            { nome: "Joao M", telefone: "5199995555", data: new Date(2024, 10, 28), operadora: $scope.operadoras[3] },
+            { nome: "Bia", telefone: "5199993333", data: new Date(2025, 5, 3), operadora: $scope.operadoras[1] },
+            { nome: "Eduardo", telefone: "5199994444", data: new Date(2024, 7, 19), operadora: $scope.operadoras[2] },
+            { nome: "Carla", telefone: "5199811111", data: new Date(2025, 2, 7), operadora: $scope.operadoras[0] },
+            { nome: "Diego", telefone: "5199822222", data: new Date(2024, 11, 15), operadora: $scope.operadoras[1] },
+            { nome: "Fernanda", telefone: "5199833333", data: new Date(2025, 3, 22), operadora: $scope.operadoras[2] },
+            { nome: "Gabriel", telefone: "5199844444", data: new Date(2025, 6, 9), operadora: $scope.operadoras[3] },
+            { nome: "Helena", telefone: "5199855555", data: new Date(2024, 9, 30), operadora: $scope.operadoras[1] },
+            { nome: "Igor", telefone: "5199866666", data: new Date(2025, 4, 18), operadora: $scope.operadoras[0] },
+            { nome: "Julia", telefone: "5199877777", data: new Date(2024, 6, 12), operadora: $scope.operadoras[2] },
+            { nome: "Lucas", telefone: "5199888888", data: new Date(2025, 1, 25), operadora: $scope.operadoras[3] },
+            { nome: "Marina", telefone: "5199899999", data: new Date(2024, 8, 5), operadora: $scope.operadoras[0] },
+            { nome: "Nicolas", telefone: "5199800000", data: new Date(2025, 7, 14), operadora: $scope.operadoras[1] },
+            { nome: "Olivia", telefone: "5199812121", data: new Date(2024, 4, 27), operadora: $scope.operadoras[2] }
         ];
 
         $scope.adicionarContato = function (contato) {
-            $scope.contatos.push(angular.copy(contato));
+            var novoContato = angular.copy(contato);
+            novoContato.data = new Date();
+            $scope.contatos.push(novoContato);
             delete $scope.contato;
             $scope.contatoForm.$setPristine();
         };
