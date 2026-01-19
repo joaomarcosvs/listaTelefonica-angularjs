3f3f6f5d673845845597fbb86ba8df9bca2dc5e8
joaomarcosvs <joaomarcos.vieirasantos@gmail.com>
2026-01-19 11:09:09 -0300

    backend - findById adjustment

--- backend/src/main/java/com/listatelefonica/phonebook/resources/ContactController.java
+++ backend/src/main/java/com/listatelefonica/phonebook/resources/ContactController.java
@@ -1,5 +1,6 @@
 package com.listatelefonica.phonebook.resources;
 
+import com.listatelefonica.phonebook.entities.Contact;
 import com.listatelefonica.phonebook.resources.dto.ContactResponse;
 import com.listatelefonica.phonebook.service.ContactService;
 
@@ -7,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,11 @@ public class ContactController {
     @GetMapping
     public ResponseEntity<List<ContactResponse>> findAll() {
         return ResponseEntity.ok(contactService.listAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Contact> findById(@PathVariable Long id){
+        Contact obj = contactService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- backend/src/main/java/com/listatelefonica/phonebook/resources/OperatorController.java
+++ backend/src/main/java/com/listatelefonica/phonebook/resources/OperatorController.java
@@ -1,11 +1,13 @@
 package com.listatelefonica.phonebook.resources;
 
+import com.listatelefonica.phonebook.entities.Operator;
 import com.listatelefonica.phonebook.resources.dto.OperatorResponse;
 import com.listatelefonica.phonebook.service.OperatorService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/operators")
 public class OperatorController {
 
+    @Autowired
     private final OperatorService operatorService;
 
     @Autowired
@@ -23,5 +26,11 @@ public class OperatorController {
     @GetMapping
     public ResponseEntity<List<OperatorResponse>> findAll() {
         return ResponseEntity.ok(operatorService.listAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Operator> findById(@PathVariable Long id){
+        Operator obj = operatorService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
