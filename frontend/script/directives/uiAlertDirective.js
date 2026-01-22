50d15abfb7bd187dd213861bfe9de62915fa78f0
joaomarcosvs <joaomarcos.vieirasantos@gmail.com>
2026-01-22 11:58:21 -0300

    frontend - directives adjustment

--- frontend/script/directives/uiAlertDirective.js
+++ frontend/script/directives/uiAlertDirective.js
@@ -1,7 +1,34 @@
-angular.module("listaTelefonica").directive("uiAlert", function() {
+angular.module("listaTelefonica").directive("uiAlert", function () {
     return {
+        restrict: "E",
         templateUrl: "script/view/alert.html",
-        replace: true,
-        restrict: "E"
+        scope: {
+            title: "@?",
+            message: "=?",
+            type: "@?",
+            dismissible: "<?",
+            onClose: "&?"
+        },
+        controllerAs: "alert",
+        bindToController: true,
+        controller: function () {
+            var vm = this;
+
+            vm.$onInit = function () {
+                vm.type = vm.type || "error";
+                vm.title = vm.title || "Ops, aconteceu um erro!";
+                vm.dismissible = vm.dismissible !== false;
+            };
+
+            vm.close = function () {
+                if (typeof vm.onClose === "function") {
+                    vm.onClose();
+                }
+
+                if (typeof vm.message !== "undefined") {
+                    vm.message = null;
+                }
+            };
+        }
     };
 });
